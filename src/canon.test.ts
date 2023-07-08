0870eb403800b5dfaf178a500abac1b1000540da
Ira Hopkinson <irahopkinson@users.noreply.github.com>
2023-07-08 20:40:17 +1200

    Improve docs (#5)
    - bump version to publish
    - improve some tests
    - also single quotes in .md files to match .ts files

--- src/canon.test.ts
+++ src/canon.test.ts
@@ -7,16 +7,37 @@ describe('Canon', () => {
       expect(bookId).toEqual('GEN');
     });
 
-    it('should return bookId', () => {
-      const bookId = Canon.bookNumberToId(40);
-      expect(bookId).toEqual('MAT');
+    it('should return error value if not found', () => {
+      const bookId = Canon.bookNumberToId(500);
+      expect(bookId).toEqual('***');
     });
   });
 
   describe('bookNumberToEnglishName()', () => {
-    it('should return bookId', () => {
+    it('should return name', () => {
       const bookName = Canon.bookNumberToEnglishName(1);
       expect(bookName).toEqual('Genesis');
+    });
+
+    it('should return error value if not found', () => {
+      const bookName = Canon.bookNumberToEnglishName(500);
+      expect(bookName).toEqual('******');
+    });
+  });
+
+  describe('bookIdToEnglishName()', () => {
+    it('should return name', () => {
+      const bookName = Canon.bookIdToEnglishName('GEN');
+      expect(bookName).toEqual('Genesis');
+    });
+  });
+
+  describe('isObsolete()', () => {
+    it("should return whether it's obsolete or not", () => {
+      let isObsolete = Canon.isObsolete(1);
+      expect(isObsolete).toBe(false);
+      isObsolete = Canon.isObsolete(87);
+      expect(isObsolete).toBe(true);
     });
   });
 });
