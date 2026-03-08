c84648b91e1ae9756ca8cb4a59012e4d5a439682
Ira Hopkinson <ira_hopkinson@sil.org>
2026-03-09 09:33:32 +1300

    migrate Jest to Vitest
    - fix build/lint config
    - also align Node 24 types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,7 +35,6 @@ export default [
         ...globals.node,
       },
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       parser: tsParser,
       ecmaVersion: 5,
       sourceType: "commonjs",
